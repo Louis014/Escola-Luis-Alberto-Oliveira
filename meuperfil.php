29c1ae580cc1ab5803184adf42660114c854c816
Pedro Araujo <106033591+psilvagg@users.noreply.github.com>
2024-12-05 11:20:37 -0300

    upload

--- meuperfil.php
+++ meuperfil.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 include "validarlogin.php";
 
 if (!isset($_SESSION["nome"])) {
-    header("Location: index.php");
-    session_destroy();
-    exit();
+  header("Location: index.php");
+  session_destroy();
+  exit();
 }
 
 
-$sql = $pdo->query("SELECT * FROM pacientes WHERE id= ".$_SESSION['id']);
+$sql = $pdo->query("SELECT * FROM pacientes WHERE id= " . $_SESSION['id']);
 
 $lista = [];
 $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
@@ -32,55 +32,55 @@ $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
   <div class="container">
     <form class="cont-form" action="../php/atualizarperfil.php" method="POST">
       <div class="coluna">
-      <?php foreach ($lista as $a): ?>
-        <div class="form-group">
-          <label for="exampleInputNome1">Nome</label>
-          <input type="text" class="form-control" name="nome" id="exampleInputEmail1" placeholder="Nome" disabled value="<?=$a['nome']; ?>" required>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputNome1">Sobrenome</label>
-          <input type="text" class="form-control" name="sobrenome" id="exampleInputEmail1" placeholder="Sobrenome" disabled value="<?=$a['sobrenome']; ?>" 
-            required>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Data de nascimento</label>
-          <input type="date" class="form-control" name="data" id="exampleInputData1" placeholder="Data de nascimento" disabled value="<?=$a['data_nascimento']; ?>" 
-            required>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Endereço de e-mail</label>
-          <input type="email" class="form-control" name="email" id="exampleInputEmail1" placeholder="Email" value="<?=$a['email']; ?>"  required>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Telefone</label>
-          <input type="number" class="form-control" name="telefone" minlength="11" maxlength="11"
-            id="exampleInputTelefone1" placeholder="Telefone" value="<?=$a['telefone']; ?>"  required>
-        </div>
+        <?php foreach ($lista as $a): ?>
+          <div class="form-group">
+            <label for="exampleInputNome1">Nome</label>
+            <input type="text" class="form-control" name="nome" id="exampleInputEmail1" placeholder="Nome" disabled value="<?= $a['nome']; ?>" required>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputNome1">Sobrenome</label>
+            <input type="text" class="form-control" name="sobrenome" id="exampleInputEmail1" placeholder="Sobrenome" disabled value="<?= $a['sobrenome']; ?>"
+              required>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Data de nascimento</label>
+            <input type="date" class="form-control" name="data" id="exampleInputData1" placeholder="Data de nascimento" disabled value="<?= $a['data_nascimento']; ?>"
+              required>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Endereço de e-mail</label>
+            <input type="email" class="form-control" name="email" id="exampleInputEmail1" placeholder="Email" value="<?= $a['email']; ?>" required>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Telefone</label>
+            <input type="number" class="form-control" name="telefone" minlength="11" maxlength="11"
+              id="exampleInputTelefone1" placeholder="Telefone" value="<?= $a['telefone']; ?>" required>
+          </div>
       </div>
       <div class="coluna">
         <div class="form-group">
           <label for="exampleInputEmail1">Endereço</label>
-          <input type="text" class="form-control" name="endereco" id="exampleInputEndereco1" placeholder="Endereço" value="<?=$a['endereco']; ?>" 
+          <input type="text" class="form-control" name="endereco" id="exampleInputEndereco1" placeholder="Endereço" value="<?= $a['endereco']; ?>"
             required>
         </div>
         <div class="form-group">
           <label for="exampleInputPassword1">Sexo</label>
-          <input type="text" class="form-control" name="sexo" id="exampleInputEndereco1" placeholder="Sexo" disabled value="<?=$a['sexo']; ?>" 
+          <input type="text" class="form-control" name="sexo" id="exampleInputEndereco1" placeholder="Sexo" disabled value="<?= $a['sexo']; ?>"
             required>
         </div>
         <div class="form-group">
           <label for="exampleInputSenha1">Senha</label>
-          <input type="password" class="form-control" name="senha" id="exampleInputEmail1" placeholder="Senha" value="<?=$a['passw']; ?>"required>
+          <input type="password" class="form-control" name="senha" id="exampleInputEmail1" placeholder="Senha" value="<?= $a['passw']; ?>" required>
         </div>
         <div class="form-group">
           <label for="exampleInputSenha1">Confirme sua senha</label>
-          <input type="password" class="form-control" name="confsenha" id="exampleInputEmail1" value="<?=$a['passw']; ?>"
+          <input type="password" class="form-control" name="confsenha" id="exampleInputEmail1" value="<?= $a['passw']; ?>"
             placeholder="Insira novamente e sua senha" required>
         </div>
-        <?php endforeach; ?> 
-        <div class="contBotao">
-          <button type="submit" class="btnCadastrarse">Atualizar dados</button>
-        </div>
+      <?php endforeach; ?>
+      <div class="contBotao">
+        <button type="submit" class="btnCadastrarse">Atualizar dados</button>
+      </div>
       </div>
     </form>
     <a href="menu.php"><button class="btnCadastrarse">Voltar</button></a>
--- novacon.php
+++ novacon.php
@@ -1,21 +1,20 @@
 <?php
-    include "../db/config.php";
-    include "../php/validarlogin.php";
+include "../db/config.php";
+include "../php/validarlogin.php";
 
-   $datacon = $_POST["datacon"] ?? "";
-   $hora = $_POST["hora"];
-   $especialidade = $_POST["especialidade"];
-   $idpaciente = $_SESSION['id'];
+$datacon = $_POST["datacon"] ?? "";
+$hora = $_POST["hora"];
+$especialidade = $_POST["especialidade"];
+$idpaciente = $_SESSION['id'];
 
-    
-        $sql = $pdo->prepare("INSERT INTO consultas (data_consulta, hora_consulta, especialidade, fk_idpaciente) 
+
+$sql = $pdo->prepare("INSERT INTO consultas (data_consulta, hora_consulta, especialidade, fk_idpaciente) 
                             VALUES (:datacon, :hora, :especialidade, :idpaciente)");
-                $sql->bindValue(":datacon", $datacon);
-                $sql->bindValue(":hora", $hora);
-                $sql->bindValue(":especialidade", $especialidade);
-                $sql->bindValue(":idpaciente", $idpaciente);
-                $sql->execute();
+$sql->bindValue(":datacon", $datacon);
+$sql->bindValue(":hora", $hora);
+$sql->bindValue(":especialidade", $especialidade);
+$sql->bindValue(":idpaciente", $idpaciente);
+$sql->execute();
 
-                header("Location: ../pages/consulagendada.html");
-                exit();
-?>
+header("Location: ../pages/consulagendada.html");
+exit();
